48c79da5b534a2f95d94c50c84bd78c59b362124
libaolei007 <821342102@qq.com>
2019-06-04 21:36:18 +0800

    测试和资源文件提交

---
+++ src/main/resources/sql/account.md
@@ -0,0 +1,4 @@
+selectAccountByName
+===
+*根据name获account
+    select * from account where name= #name#
---
+++ src/test/java/org/lbl/springboot/demo/SpringbootRedisApplicationTests.java
@@ -0,0 +1,26 @@
+package org.lbl.springboot.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.lbl.springboot.demo.t9.RedisDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootRedisApplicationTests {
+    public static Logger logger = LoggerFactory.getLogger(SpringbootRedisApplicationTests.class);
+    @Autowired
+    RedisDao redisDao;
+
+    @Test
+    public void testRedis() {
+        redisDao.setKey("name", "libl");
+        redisDao.setKey("age", "11");
+        logger.info(redisDao.getValue("name"));
+        logger.info(redisDao.getValue("age"));
+    }
+}
